50eedbcc8dedc10ef435744d1c525455e53f47ac
MarcosMarossi96 <marcos_marossi@hotmail.com>
2024-04-04 13:21:21 -0300

    Removing PersonAPI

--- PersonAPI/src/main/java/br/com/person/core/MediaType.java
+++
@@ -1,9 +0,0 @@
-package br.com.person.core;
-
-public class MediaType {
-
-	public static final String APPLICATION_YML = "application/x-yaml";
-	public static final String APPLICATION_JSON = "application/json";
-	public static final String APPLICATION_XML = "application/xml";
-
-}
--- PersonAPI/src/main/resources/db/migration/V1__create-person.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS person (
-  id bigint NOT NULL AUTO_INCREMENT,
-  first_name varchar(100) NOT NULL,
-  last_name varchar(125) NOT NULL,  
-  address varchar(255) NOT NULL,  
-  gender varchar(10) NOT NULL,  
-  PRIMARY KEY (id)
-);
